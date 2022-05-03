c6b759891d06f07e9126dc94293d1ce9378a5769
Rango <39035567+YungSpecht@users.noreply.github.com>
2022-05-03 23:44:21 +0200

    fix rk2

--- core/src/com/rock/golf/Math/RK2Solver.java
+++ core/src/com/rock/golf/Math/RK2Solver.java
@@ -45,8 +45,8 @@ public class RK2Solver {
         double formulaY;
 
         if(vector.getXSpeed() == 0 && vector.getYSpeed() == 0){
-            formulaX = (-PhysicsEngine.g * xSlope) - uK * PhysicsEngine.g * (xSlope / Math.sqrt(Math.pow(xSlope, 2) + Math.pow(ySlope, 2)));
-            formulaY = (-PhysicsEngine.g * ySlope) - uK * PhysicsEngine.g * (ySlope / Math.sqrt(Math.pow(xSlope, 2) + Math.pow(ySlope, 2)));
+            formulaX = (-PhysicsEngine.g * xSlope) - uS * PhysicsEngine.g * (xSlope / Math.sqrt(Math.pow(xSlope, 2) + Math.pow(ySlope, 2)));
+            formulaY = (-PhysicsEngine.g * ySlope) - uS * PhysicsEngine.g * (ySlope / Math.sqrt(Math.pow(xSlope, 2) + Math.pow(ySlope, 2)));
         }
         else{
             formulaX = (-PhysicsEngine.g * xSlope) - uK * PhysicsEngine.g * (vector.getXSpeed() / Math.sqrt(Math.pow(vector.getXSpeed(), 2) + Math.pow(vector.getYSpeed(), 2)));
