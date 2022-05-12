772b3536da6248854d2aa7911d9d64104ddeea40
Akku Ram <reachakhil10@gmail.com>
2022-05-12 19:23:58 +0200

    commit new

---
+++ TestingMethods/RK2Test.java
@@ -0,0 +1,13 @@
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
+
+public class RK2Test {
+
+    @Test
+    void input(){
+        Function golfcourse = InputModule.get_profile;
+        RK2Solver solve = new RK2Solver(0.1, 0.2, 0.1, golfCourse);
+        StateVector vector = new StateVector(1.825, 0, -1.7903, 0);
+        assertEquals(vector, solve.runge_kutta_two(vector));
+    }
+}
