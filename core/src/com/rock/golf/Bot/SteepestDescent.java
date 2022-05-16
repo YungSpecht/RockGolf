9bedb4c2a68a0855cf7b77be5e399f6c43263368
Rango <39035567+YungSpecht@users.noreply.github.com>
2022-05-16 11:58:14 +0200

    cleaned up hill climb

--- core/src/com/rock/golf/Bot/SteepestDescent.java
+++ core/src/com/rock/golf/Bot/SteepestDescent.java
@@ -27,65 +27,55 @@ public class SteepestDescent {
         currentShot = scale_velocity(new double[]{xdiff, ydiff}, 4);
         currentShotCoords = engine.get_shot(currentShot[0], currentShot[1]);
         currentShotDistance = euclidian_distance(currentShotCoords);
+
+        System.out.println("+++FIRST LOOP+++");
+
         int counter = 0;
-        while(currentShotDistance >targetRad && counter < 5){
+        while(currentShotDistance >= targetRad && counter < 5){
             mountain_climber(0.5 - (0.1*counter));
-            if(currentShotDistance < targetRad){
-                return currentShot;
-            }
             System.out.println("Iteration: " + ++counter);
         }
         System.out.println("+++SECOND LOOP+++");
         counter = 0;
-        while(currentShotDistance >targetRad && counter < 5){
-            mountain_climber(0.1 - (0.02*counter));
-            if(currentShotDistance < targetRad){
-                return currentShot;
-            }
+        while(currentShotDistance >targetRad && counter < 4){
+            mountain_climber(0.08 - (0.02*counter));
             System.out.println("Iteration: " + ++counter);
         }
-
+        System.out.println("+++THIRD LOOP+++");
+        counter = 0;
+        while(currentShotDistance >targetRad && counter < 5){
+            mountain_climber(0.01 - (0.002*counter));
+            System.out.println("Iteration: " + ++counter);
+        }
         return currentShot;
     }
 
     private void mountain_climber(double precision){
         boolean successorAvailable;
         do{
-            double[] successor1 = new double[]{currentShot[0] - precision, currentShot[1]};
-            double[] successor2 = new double[]{currentShot[0] + precision, currentShot[1]};
-            double[] successor3 = new double[]{currentShot[0], currentShot[1] - precision};
-            double[] successor4 = new double[]{currentShot[0], currentShot[1] + precision};
+            double[][] successors = new double[4][2];
+            successors[0] = new double[]{currentShot[0] - precision, currentShot[1]};
+            successors[1] = new double[]{currentShot[0] + precision, currentShot[1]};
+            successors[2] = new double[]{currentShot[0], currentShot[1] - precision};
+            successors[3] = new double[]{currentShot[0], currentShot[1] + precision};
 
-            double[] successor1Coords = null;
-            double[] successor2Coords = null;
-            double[] successor3Coords = null;
-            double[] successor4Coords = null;
-
-            if(vel_is_legal(successor1)){
-                successor1Coords = engine.get_shot(successor1[0], successor1[1]);
-            }
-            if(vel_is_legal(successor2)){
-                successor2Coords = engine.get_shot(successor2[0], successor2[1]);
-            }
-            if(vel_is_legal(successor3)){
-                successor3Coords = engine.get_shot(successor3[0], successor3[1]);
-            }
-            if(vel_is_legal(successor4)){
-                successor4Coords = engine.get_shot(successor4[0], successor4[1]);
-            }
-
-            int bestSuccessor = compare_successors(successor1Coords, successor2Coords, successor3Coords, successor4Coords);
-
-            if(bestSuccessor > 0){
-                switch(bestSuccessor){
-                    case 1 : currentShot = successor1; currentShotCoords = successor1Coords; currentShotDistance = euclidian_distance(currentShotCoords); break;
-                    case 2 : currentShot = successor2; currentShotCoords = successor2Coords; currentShotDistance = euclidian_distance(currentShotCoords); break;
-                    case 3 : currentShot = successor3; currentShotCoords = successor3Coords; currentShotDistance = euclidian_distance(currentShotCoords); break;
-                    case 4 : currentShot = successor4; currentShotCoords = successor4Coords; currentShotDistance = euclidian_distance(currentShotCoords); break;
+            double[][] successorCoords = new double[successors.length][2];
+            for(int i = 0; i < successors.length; i++){
+                if(vel_is_legal(successors[i])){
+                    successorCoords[i] = engine.get_shot(successors[i][0], successors[i][1]);
                 }
-                successorAvailable = true;
+                else{
+                    successorCoords[i] = null;
+                }
             }
-            else{
+            int bestSuccessor = compare_successors(successorCoords);
+
+            if(bestSuccessor > -1){
+                currentShot = successors[bestSuccessor];
+                currentShotCoords = successorCoords[bestSuccessor];
+                currentShotDistance = euclidian_distance(currentShotCoords);
+                successorAvailable = true;
+            }else{
                 successorAvailable = false;
             }
             if(currentShotDistance < targetRad){
@@ -95,39 +85,17 @@ public class SteepestDescent {
         }while(successorAvailable);
     }
 
-    private int compare_successors( double[] one, double[] two, double[] three, double[] four){
+    private int compare_successors(double[][] successorCoords){
         double reference = currentShotDistance;
-        int result = 0;
-        if(one != null && euclidian_distance(one) < reference){
-            result = 1;
-            reference = euclidian_distance(one);
-            System.out.println("New Shortest Distance: " + reference);
-            if(reference < targetRad){
-                return result;
-            }
-        }
-        if(two != null && euclidian_distance(two) < reference){
-            result = 2;
-            reference = euclidian_distance(two);
-            System.out.println("New Shortest Distance: " + reference);
-            if(reference < targetRad){
-                return result;
-            }
-        }
-        if(three != null && euclidian_distance(three) < reference){
-            result = 3;
-            reference = euclidian_distance(three);
-            System.out.println("New Shortest Distance: " + reference);
-            if(reference < targetRad){
-                return result;
-            }
-        }
-        if(four != null && euclidian_distance(four) < reference){
-            result = 4;
-            reference = euclidian_distance(four);
-            System.out.println("New Shortest Distance: " + reference);
-            if(reference < targetRad){
-                return result;
+        int result = -1;
+        for(int i = 0; i < successorCoords.length; i++){
+            if(successorCoords[i] != null && euclidian_distance(successorCoords[i]) < reference){
+                result = i;
+                reference = euclidian_distance(successorCoords[i]);
+                System.out.println("New Shortest Distance: " + (reference - targetRad));
+                if(reference < targetRad){
+                    return result;
+                }
             }
         }
         return result;
@@ -145,6 +113,5 @@ public class SteepestDescent {
         double currentVel = Math.sqrt(Math.pow(velocities[0], 2) + Math.pow(velocities[1], 2));
         double scalar = velocity / currentVel;
         return new double[] { velocities[0] * scalar, velocities[1] * scalar };
-
     }
 }
