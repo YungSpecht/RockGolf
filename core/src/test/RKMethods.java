272c4ee32cb7d4ed29ad99e95f268cd33f529658
Akku Ram <reachakhil10@gmail.com>
2022-05-20 18:38:50 +0200

    commit new

--- core/src/test/RKMethods.java
+++ core/src/test/RKMethods.java
@@ -12,4 +12,11 @@ public class RKMethods {
         RK2Solver solve = new RK2Solver(0.1, 0.2, 0.1, golfcourse);
         return solve.compute_step(v1);
     }
+
+    public StateVector Test_For_Flat_Surface_RK4(){
+        Function golfcourse = new Function("h(x,y)=0");
+        StateVector v1 = new StateVector(0, 0, 1, 0);
+        RK2Solver solve = new RK2Solver(0.1, 0.2, 0.1, golfcourse);
+        return solve.compute_step(v1);
+    }
 }
--- desktop/src/NumericalTests.java
+++ desktop/src/NumericalTests.java
@@ -17,6 +17,13 @@ public class NumericalTests {
         assertEquals(vector, testedVector);
     }
 
+    @Test
+    public void Test_For_Flat_Surface_RK4(){
+        StateVector vector = new StateVector(0.23038,0,0.9019,0);
+        StateVector testedVector = computationEngine.Test_For_Flat_Surface_RK4();
+        assertEquals(vector, testedVector);
+    }
+
 
 
 }
