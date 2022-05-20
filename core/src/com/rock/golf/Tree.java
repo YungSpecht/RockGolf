ac16f5daca9f4de43d0d00efea4e189528cae5db
Fred <fj.schmitz@student.maastrichtuniversity.nl>
2022-05-20 11:32:04 +0200

    Create Tree.java

---
+++ core/src/com/rock/golf/Tree.java
@@ -0,0 +1,27 @@
+package com.rock.golf;
+
+public class Tree {
+
+    private double position[];
+    private double radius = 3;
+
+    public Tree(double position[], double radius) {
+        this.position = position;
+        this.radius = radius;
+    }
+
+    public boolean collision_with_tree(double xPos, double yPos) {
+        if (Math.pow(xPos - position[0], 2) + Math.pow(yPos - position[1], 2) <= Math.pow(radius, 2)) {
+            return true;
+        }
+        return false;
+    }
+
+    public double[] get_position() {
+        return position;
+    }
+
+    public double get_radius() {
+        return radius;
+    }
+}
