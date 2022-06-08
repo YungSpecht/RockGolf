146a8156dc0599f1eba421758a6002eab41ca88a
Fred <fj.schmitz@student.maastrichtuniversity.nl>
2022-06-08 13:31:38 +0200

    obstacle change

--- core/src/com/rock/golf/obstacleCreator.java
+++ core/src/com/rock/golf/obstacleCreator.java
@@ -27,13 +27,27 @@ public class obstacleCreator implements InputProcessor {
 
         golf.switchToObstacle();
 
-        // asdlkfj
-
         if (keycode == Input.Keys.R) {
-            new Obstacle(obstacle.getPosition(), 150, 50);
+
+            double position[] = new double[2];
+
+            double mouseX = MouseInfo.getPointerInfo().getLocation().getX();
+            double mouseY = MouseInfo.getPointerInfo().getLocation().getY();
+
+            // pseudo obstacle used to be able to see where you place the obstacle in the
+            // end
+            ShapeRenderer Rectangle = new ShapeRenderer();
+            Rectangle.begin(ShapeRenderer.ShapeType.Filled);
+            Rectangle.setColor(Color.BLACK);
+            Rectangle.rect((float) mouseX, (float) mouseY, 150, 50);
+
+            position[0] = mouseX;
+            position[1] = mouseY;
+
+            new Obstacle(position, 150, 50);
 
         } else if (keycode == Input.Keys.T) {
-            new tre
+
         }
         return false;
     }
