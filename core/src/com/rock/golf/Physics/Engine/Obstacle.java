0c77cbeadf9fea02bef756b86d37cb68fa6e2715
Weronika <w.wlazo@student.maastrichtuniversity.nl>
2022-06-08 10:48:21 +0200

    obstacle

---
+++ core/src/com/rock/golf/Physics/Engine/Obstacle.java
@@ -0,0 +1,56 @@
+package com.rock.golf.Physics.Engine;
+
+public class Obstacle {
+    
+    private double [] position;
+    private double width;
+    private double height;
+
+    /**
+     * Creates a new obstacele
+     * @param position specifies x and y coordinates of left lower corner of a square obstacle
+     * @param length specifies length of sides
+     */
+    public Obstacle(double[] position, double width, double height){
+        this.width = width;
+        this.height = height;
+        this.position = position;
+    }
+
+    /**
+     * Verifies whether ball collided with the obstacle
+     * @param xPos position of the ball
+     * @param yPos position of the ball
+     * @param radius radius of the ball
+     * @return
+     */
+    public boolean obstacleCollision(double xPos, double yPos, double radius){
+        if((xPos+radius)>=position[0] || (xPos-radius)<=position[0] + width ){
+            return true;
+        } else if((yPos+radius)>=position[1] || (yPos-radius)<=position[0] + height ){
+            return true;
+        }else return false;
+
+    }
+
+    public double[] bounce(double xPos, double yPos, double radius,double xVelocity, double yVelocity){
+        if((xPos+radius)>=position[0] || (xPos-radius)<=position[0] + width ){
+           return new double []{-xVelocity, yVelocity};
+        } else if((yPos+radius)>=position[1] || (yPos-radius)<=position[0] + height ){
+            return new double []{xVelocity, -yVelocity};
+        } else return new double[] {xVelocity, yVelocity};
+        
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double[] getPosition() {
+        return position;
+    }
+}
