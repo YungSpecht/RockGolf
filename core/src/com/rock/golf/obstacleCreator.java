92eb0dfdc4fe32498c81211b88d6442e4de83dec
Walid el akel <91736545+Lordwalid@users.noreply.github.com>
2022-06-12 21:02:54 +0200

    nothing

--- core/src/com/rock/golf/obstacleCreator.java
+++ core/src/com/rock/golf/obstacleCreator.java
@@ -2,10 +2,14 @@ package com.rock.golf;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.rock.golf.Physics.Engine.Obstacle;
 import com.rock.golf.Physics.Engine.PhysicsEngine;
 import com.rock.golf.Physics.Engine.Tree;
 import com.rock.golf.Physics.Engine.rectangleObstacle;
+
 
 public class obstacleCreator implements InputProcessor {
 
