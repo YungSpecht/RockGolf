974da8861b50312814fb5530971aa32161e9a7d5
Akku Ram <reachakhil10@gmail.com>
2022-06-12 17:54:11 +0200

    Small fix

--- core/src/com/rock/golf/Physics/Solvers/AdamsBashforth2.java
+++ core/src/com/rock/golf/Physics/Solvers/AdamsBashforth2.java
@@ -3,6 +3,7 @@ package com.rock.golf.Physics.Solvers;
 import com.rock.golf.Physics.Engine.StateVector;
 import org.mariuszgromada.math.mxparser.*;
 
+
 public class AdamsBashforth2 extends Solver{
 
     RK2Solver solve = new RK2Solver(uK, uS, h, golfCourse);
@@ -16,6 +17,9 @@ public class AdamsBashforth2 extends Solver{
         StateVector initialValue = solve.computeStep(current);
         StateVector AdamsPredictor = StateVector.substract(StateVector.add(current, StateVector.multiply(function(initialValue), (3.0 / 2.0)*h)),  StateVector.multiply(function(current), (1.0 / 2.0)*h));
         StateVector AdamsCorrector = StateVector.substract(StateVector.add(StateVector.add(current, StateVector.multiply(function(AdamsPredictor), (5.0 / 12.0)*h)), StateVector.multiply(function(initialValue), (8.0/12.0)*h)),  StateVector.multiply(function(current), (1.0 / 2.0)*h));
+        current = initialValue;
+        initialValue = AdamsPredictor;
         return AdamsCorrector;
     }
+
 }
