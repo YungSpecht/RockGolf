3e73615f4a12ae85255d4fb13e31574056a0659e
Rango <39035567+YungSpecht@users.noreply.github.com>
2022-06-15 12:02:38 +0200

    fix and progress

--- core/src/com/rock/golf/Bot/PathBot.java
+++ core/src/com/rock/golf/Bot/PathBot.java
@@ -2,11 +2,27 @@ package com.rock.golf.Bot;
 
 import java.util.ArrayList;
 
+import com.rock.golf.Pathfinding.Node;
+import com.rock.golf.Pathfinding.NodeFinder;
+import com.rock.golf.Physics.Engine.PhysicsEngine;
+import com.rock.golf.Physics.Engine.StateVector;
+
 public class PathBot extends Bot{
+    private ArrayList<Node> path;
+    private NodeFinder finder;
+
+    public PathBot(PhysicsEngine engine, ArrayList<Node> path){
+        this.engine = engine;
+        this.path = path;
+        finder = new NodeFinder(path, engine);
+    }
 
     @Override
     public double[] getMove() {
-        // TODO Auto-generated method stub
+        StateVector current = engine.getVector();
+        Node angleOfAttack = finder.findNextTargetNode(current.getXPos(), current.getYPos());
+
+        
         return null;
     }
     
